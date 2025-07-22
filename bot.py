5c4f8256ee410822c03893dbcc1745c9f53734db
$eed <tjdeff@gmail.com>
2025-07-22 09:33:56 -0600

    .

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-#Required libraries imports
+#Required libraries & imports
 ##################################################################################
 import discord
 from discord.ext import commands
 from discord import app_commands
 import json
 import os
-##################################################################################
+
 load_dotenv()
+##################################################################################
+
 # Load JSON data
 with open("blueprints.json", "r") as f:
     data = json.load(f)
